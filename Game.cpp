7b01b60d6431a77281bf23a09498799014653a46
Bruno Opsenica <bruno.opsenica@gmail.com>
2019-11-13 16:13:37 -0500

    Initial commit

---
+++ Game.cpp
@@ -0,0 +1,258 @@
+//
+// Game.cpp
+//
+
+#include "pch.h"
+#include "Game.h"
+
+extern void ExitGame();
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+using Microsoft::WRL::ComPtr;
+
+Game::Game() noexcept(false)
+{
+    m_deviceResources = std::make_unique<DX::DeviceResources>();
+    m_deviceResources->RegisterDeviceNotify(this);
+}
+
+// Initialize the Direct3D resources required to run.
+void Game::Initialize(HWND window, int width, int height)
+{
+    m_deviceResources->SetWindow(window, width, height);
+
+    m_deviceResources->CreateDeviceResources();
+    CreateDeviceDependentResources();
+
+    m_deviceResources->CreateWindowSizeDependentResources();
+    CreateWindowSizeDependentResources();
+
+    // TODO: Change the timer settings if you want something other than the default variable timestep mode.
+    // e.g. for 60 FPS fixed timestep update logic, call:
+    /*
+    m_timer.SetFixedTimeStep(true);
+    m_timer.SetTargetElapsedSeconds(1.0 / 60);
+    */
+}
+
+#pragma region Frame Update
+// Executes the basic game loop.
+void Game::Tick()
+{
+    m_timer.Tick([&]() {
+        Update(m_timer);
+        });
+
+    Render();
+}
+
+// Updates the world.
+void Game::Update(DX::StepTimer const& timer)
+{
+    float frameTime = float(timer.GetElapsedSeconds());
+    float totalTime = float(timer.GetTotalSeconds());
+
+    m_world = Matrix::CreateRotationY(cosf(static_cast<float>(totalTime)));
+}
+#pragma endregion
+
+#pragma region Frame Render
+// Draws the scene.
+void Game::Render()
+{
+    // Don't try to render anything before the first Update.
+    if (m_timer.GetFrameCount() == 0) {
+        return;
+    }
+
+    Clear();
+
+    m_deviceResources->PIXBeginEvent(L"Render");
+    auto context = m_deviceResources->GetD3DDeviceContext();
+
+    // TODO: Add your rendering code here.
+    context->OMSetBlendState(m_states->Opaque(), nullptr, 0xFFFFFFFF);
+    context->OMSetDepthStencilState(m_states->DepthNone(), 0);
+    context->RSSetState(m_rasterState.Get());
+
+    m_effect->SetWorld(m_world);
+
+    m_effect->Apply(context);
+
+    context->IASetInputLayout(m_inputLayout.Get());
+
+    Vector3 xaxis(2.f, 0.f, 0.f);
+    Vector3 yaxis(0.f, 0.f, 2.f);
+    Vector3 origin = Vector3::Zero;
+
+    m_batch->Begin();
+    size_t divisions = 20;
+    for (size_t i = 0; i <= divisions; ++i) {
+        float fPercent = float(i) / float(divisions);
+        fPercent = (fPercent * 2.0f) - 1.0f;
+        Vector3 scale = xaxis * fPercent + origin;
+
+        VertexPositionColor v1{ scale - yaxis, Colors::White };
+        VertexPositionColor v2{ scale + yaxis, Colors::White };
+        m_batch->DrawLine(v1, v2);
+    }
+
+    for (size_t i = 0; i <= divisions; ++i) {
+        float fPercent = float(i) / float(divisions);
+        fPercent = (fPercent * 2.0f) - 1.0f;
+
+        Vector3 scale = yaxis * fPercent + origin;
+
+        VertexPositionColor v1{ scale - xaxis, Colors::White };
+        VertexPositionColor v2{ scale + xaxis, Colors::White };
+        m_batch->DrawLine(v1, v2);
+    }
+    m_batch->End();
+
+    m_deviceResources->PIXEndEvent();
+
+    // Show the new frame.
+    m_deviceResources->Present();
+}
+
+// Helper method to clear the back buffers.
+void Game::Clear()
+{
+    m_deviceResources->PIXBeginEvent(L"Clear");
+
+    // Clear the views.
+    auto context = m_deviceResources->GetD3DDeviceContext();
+    auto renderTarget = m_deviceResources->GetRenderTargetView();
+    auto depthStencil = m_deviceResources->GetDepthStencilView();
+
+    context->ClearRenderTargetView(renderTarget, Colors::CornflowerBlue);
+    context->ClearDepthStencilView(depthStencil, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+    context->OMSetRenderTargets(1, &renderTarget, depthStencil);
+
+    // Set the viewport.
+    auto viewport = m_deviceResources->GetScreenViewport();
+    context->RSSetViewports(1, &viewport);
+
+    m_deviceResources->PIXEndEvent();
+}
+#pragma endregion
+
+#pragma region Message Handlers
+// Message handlers
+void Game::OnActivated()
+{
+    // TODO: Game is becoming active window.
+}
+
+void Game::OnDeactivated()
+{
+    // TODO: Game is becoming background window.
+}
+
+void Game::OnSuspending()
+{
+    // TODO: Game is being power-suspended (or minimized).
+}
+
+void Game::OnResuming()
+{
+    m_timer.ResetElapsedTime();
+
+    // TODO: Game is being power-resumed (or returning from minimize).
+}
+
+void Game::OnWindowMoved()
+{
+    auto r = m_deviceResources->GetOutputSize();
+    m_deviceResources->WindowSizeChanged(r.right, r.bottom);
+}
+
+void Game::OnWindowSizeChanged(int width, int height)
+{
+    if (!m_deviceResources->WindowSizeChanged(width, height))
+        return;
+
+    CreateWindowSizeDependentResources();
+
+    // TODO: Game window is being resized.
+}
+
+// Properties
+void Game::GetDefaultSize(int& width, int& height) const
+{
+    // TODO: Change to desired default window size (note minimum size is 320x200).
+    width = 800;
+    height = 600;
+}
+#pragma endregion
+
+#pragma region Direct3D Resources
+// These are the resources that depend on the device.
+void Game::CreateDeviceDependentResources()
+{
+    ID3D11Device1* device = m_deviceResources->GetD3DDevice();
+
+    CD3D11_RASTERIZER_DESC rastDesc{
+        D3D11_FILL_SOLID, D3D11_CULL_NONE, FALSE,
+        D3D11_DEFAULT_DEPTH_BIAS,
+        D3D11_DEFAULT_DEPTH_BIAS_CLAMP,
+        D3D11_DEFAULT_SLOPE_SCALED_DEPTH_BIAS,
+        TRUE, FALSE, TRUE, FALSE,
+    };
+    DX::ThrowIfFailed(device->CreateRasterizerState(&rastDesc, m_rasterState.ReleaseAndGetAddressOf()));
+    m_states = std::make_unique<CommonStates>(device);
+
+    m_effect = std::make_unique<BasicEffect>(device);
+    m_effect->SetVertexColorEnabled(true);
+
+    void const* shaderByteCode;
+    size_t byteCodeLength;
+
+    m_effect->GetVertexShaderBytecode(&shaderByteCode, &byteCodeLength);
+
+    DX::ThrowIfFailed(
+        device->CreateInputLayout(
+            VertexPositionColor::InputElements,
+            VertexPositionColor::InputElementCount,
+            shaderByteCode,
+            byteCodeLength,
+            m_inputLayout.ReleaseAndGetAddressOf()
+        )
+    );
+
+    m_batch = std::make_unique<PrimitiveBatch<VertexPositionColor>>(m_deviceResources->GetD3DDeviceContext());
+
+    m_world = Matrix::Identity;
+}
+
+// Allocate all memory resources that change on a window SizeChanged event.
+void Game::CreateWindowSizeDependentResources()
+{
+    m_view = Matrix::CreateLookAt(Vector3{ 2.0f, 2.0f, 2.0f }, Vector3::Zero, Vector3::UnitY);
+    RECT viewPort = m_deviceResources->GetOutputSize();
+    float width = float(viewPort.right - viewPort.left);
+    float height = float(viewPort.bottom - viewPort.top);
+    m_proj = Matrix::CreatePerspectiveFieldOfView(XM_PI / 4.0f, width / height, 0.1f, 10.f);
+
+    m_effect->SetView(m_view);
+    m_effect->SetProjection(m_proj);
+}
+
+void Game::OnDeviceLost()
+{
+    m_rasterState.Reset();
+    m_states.reset();
+    m_effect.reset();
+    m_batch.reset();
+    m_inputLayout.Reset();
+}
+
+void Game::OnDeviceRestored()
+{
+    CreateDeviceDependentResources();
+
+    CreateWindowSizeDependentResources();
+}
+#pragma endregion
