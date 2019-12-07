787bfa279a8afdf1a261aabd1a873ab5a097d8e8
Bruno Opsenica <bruno.opsenica@gmail.com>
2019-12-07 16:04:50 -0500

    Remove some dead code from pre ecs

--- src/NodeList.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pch.h"
-
-namespace bdr
-{
-    //DirectX::SimpleMath::Matrix calcLocalTransform(const Node& node);
-    //DirectX::SimpleMath::Matrix processGlobalTransform(const NodeList& nodeList, int32_t nodeIdx);
-    //void updateNodes(NodeList& nodeList);
-}
