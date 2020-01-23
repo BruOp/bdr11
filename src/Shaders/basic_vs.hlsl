f6081ed3983f5298f77e968f4d7317f6b80944c0
Bruno Opsenica <bruno.opsenica@gmail.com>
2020-01-22 21:25:09 -0500

    Restore 'basic' shaders

--- src/Shaders/basic_vs.hlsl
+++ src/Shaders/basic_vs.hlsl
@@ -3,7 +3,6 @@ cbuffer VIEW_CONSTANT_BUFFER : register(b0)
     matrix view;
     matrix proj;
     matrix VP;
-    float4 cameraPos;
 }
 
 cbuffer DRAW_CONSTANTS_BUFFER : register(b1)
@@ -16,25 +15,20 @@ struct VSInput
 {
     float3 Position : SV_Position;
     float3 Normal : NORMAL;
-    float2 UV : TEXCOORD;
 };
 
 struct VSOutput
 {
     float4 PositionCS : SV_Position;
-    float3 PositionWS : POSITIONWS;
     float3 NormalWS : NORMALWS;
-    float2 vUV : TEXCOORD;
 };
 
 VSOutput main(in VSInput input)
 {
     VSOutput output;
-    output.PositionWS = mul(float4(input.Position, 1.0f), model).xyz;
-    output.PositionCS = mul(float4(output.PositionWS, 1.0f), VP);
-    output.vUV = input.UV;
-    
-    output.NormalWS = normalize(mul(float4(input.Normal, 1.0f), invModel).xyz);
+    output.PositionCS = mul(mul(float4(input.Position, 1.0f), model), VP);
+
+    output.NormalWS = mul(float4(input.Normal, 1.0f), invModel).xyz;
 
     return output;
 }
